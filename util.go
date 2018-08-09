b75a33a92c0a662f564cd23174662ee7aeee3972
Sriram Damodharan <sdamodharan@thapovan-inc.com>
2018-08-09 17:11:14 +0530

    Added util wrapper methods to marshal and unmarshal protobuf messages & json

--- util.go
+++ util.go
@@ -1,1 +1,24 @@
 package orionproto
+
+import (
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	"io"
+)
+
+var marshaler = &jsonpb.Marshaler {
+	EmitDefaults: true,
+	OrigName: true,
+}
+
+var unMarshaler = &jsonpb.Unmarshaler{
+	AllowUnknownFields: true,
+}
+
+func ProtoToJson(protoMessage proto.Message) (string,error)  {
+	return marshaler.MarshalToString(protoMessage)
+}
+
+func JsonToProto(r io.Reader, protoMessage proto.Message) error  {
+	return unMarshaler.Unmarshal(r,protoMessage)
+}
